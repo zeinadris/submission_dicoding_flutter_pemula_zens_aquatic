8149c72b0a173596bd11ce903ed02cb8f405a976
Zein Adri <zeinadri@gmail.com>
2022-08-18 19:59:04 +0700

    finish

--- lib/Package/list_product.dart
+++ lib/Package/list_product.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class ListProduct {
   String name;
   String description;
@@ -35,4 +33,58 @@ var listProductList = [
     price: ' : Rp 200.000',
     imageAssets: 'images/list/cencu.jpg',
   ),
+  ListProduct(
+      name: 'Louhan F3',
+      description:
+          'Kamfa F3 merupakan hasil persilangan dari jenis kamfa F2 dengan louhan jenis lain. Dan kamfa F3 merupakan ras yang bisa dibilang sudah tidak lagi kamfa, bahkan sering tubuhnya dominan cencu. Untuk memnggolonkanya bisa dilihat dari bentuk dominan acecoris tubuhnya, kalu masih menjurus kamfa bisa disebut kamfa f3. Kalu di Thailand untuk menghasilkan F3 mongkol, mereka menyilangkan antara pejantan jenis SRD atau SRM dengan indukan betina jenis F2. ',
+      price: ' : Rp 100.000',
+      imageAssets: 'images/list/f3.jpg'),
+  ListProduct(
+      name: 'Koki Ranchu',
+      description:
+          'Ranchu adalah jenis ikan mas bertudung yang berasal dari Cina. Hal ini disebut sebagai raja ikan mas oleh orang Jepang. Maruko lebih sering mengacu pada ikan mas telur-ikan., Ranchus membutuhkan 20 galon per masing-masing.',
+      price: ' : Rp 35.000',
+      imageAssets: 'images/list/ranchu.jpg'),
+  ListProduct(
+      name: 'Koki Oranda',
+      description:
+          'Oranda adalah jenis ikan mas yang dicirikan oleh "tudung" seperti gelembung yang menonjol di kepala. Pertumbuhan kepala atau tudung mungkin merupakan pertumbuhan yang menonjol di bagian atas kepala atau mungkin menutupi seluruh wajah kecuali mulut.',
+      price: ' : Rp 30.000',
+      imageAssets: 'images/list/oranda.jpeg'),
+  ListProduct(
+      name: 'Burayak Koi',
+      description:
+          'Koi atau secara spesifiknya koi berasal dari bahasa Jepang yang berarti ikan karper. Lebih spesifik lagi merujuk pada nishikigoi, yang kurang lebih bermakna ikan karper yang bersulam emas atau perak. Di Jepang, koi menjadi semacam simbol cinta dan persahabatan.',
+      price: ' : Rp 10.000',
+      imageAssets: 'images/list/babykoi.jpg'),
+  ListProduct(
+      name: 'Koi Slayer',
+      description:
+          'Koi kupu-kupu, koi sirip panjang, atau ikan mas naga adalah jenis ikan hias yang terkenal dengan siripnya yang memanjang. Ikan ini adalah jenis ikan mas biasa, Cyprinus carpio, yang mencakup banyak ras ikan mas liar serta koi peliharaan.',
+      price: ' : Rp 20.000',
+      imageAssets: 'images/list/koislayer.jpg'),
+  ListProduct(
+      name: 'Albino Yellow Cobra',
+      description:
+          'Gupi, ikan seribu, ikan cere, atau suwadakar, adalah salah satu spesies ikan hias air tawar yang paling populer di dunia. Karena mudahnya menyesuaikan diri dan beranak-pinak, di banyak tempat di Indonesia ikan ini telah menjadi ikan liar yang memenuhi parit-parit dan selokan',
+      price: ' : Rp 25.000',
+      imageAssets: 'images/list/cobra.jpg'),
+  ListProduct(
+      name: 'Albino German Yellow',
+      description:
+          'Gupi, ikan seribu, ikan cere, atau suwadakar, adalah salah satu spesies ikan hias air tawar yang paling populer di dunia. Karena mudahnya menyesuaikan diri dan beranak-pinak, di banyak tempat di Indonesia ikan ini telah menjadi ikan liar yang memenuhi parit-parit dan selokan',
+      price: ' : Rp 25.000',
+      imageAssets: 'images/list/agy.jpeg'),
+  ListProduct(
+      name: 'Ranchu Sapi',
+      description:
+          'Ranchu adalah jenis ikan mas bertudung yang berasal dari Cina. Hal ini disebut sebagai raja ikan mas oleh orang Jepang. Maruko lebih sering mengacu pada ikan mas telur-ikan., Ranchus membutuhkan 20 galon per masing-masing.',
+      price: ' : Rp 65.000',
+      imageAssets: 'images/list/sapi.jpg'),
+  ListProduct(
+      name: 'Snakeskin Blue Koi',
+      description:
+          'Gupi, ikan seribu, ikan cere, atau suwadakar, adalah salah satu spesies ikan hias air tawar yang paling populer di dunia. Karena mudahnya menyesuaikan diri dan beranak-pinak, di banyak tempat di Indonesia ikan ini telah menjadi ikan liar yang memenuhi parit-parit dan selokan',
+      price: ' : Rp 50.000',
+      imageAssets: 'images/list/bluekoi.jpg')
 ];
--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,11 +1,34 @@
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:zens_aquatic/Package/list_product.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('https://wa.me/6288802675025');
 
 class DetailPage extends StatelessWidget {
   final ListProduct listProduct;
   const DetailPage({Key? key, required this.listProduct}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      if (constraints.maxWidth > 800) {
+        return WebDetailPage(listProduct: listProduct);
+      } else {
+        return MobileDetailPage(listProduct: listProduct);
+      }
+    });
+  }
+}
+
+class MobileDetailPage extends StatelessWidget {
+  final ListProduct listProduct;
+
+  const MobileDetailPage({Key? key, required this.listProduct})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,22 +56,28 @@ class DetailPage extends StatelessWidget {
           children: <Widget>[
             Column(
               children: <Widget>[
-                Card(
-                  color: Colors.blueAccent,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Container(
-                      padding: const EdgeInsets.all(20),
-                      child: Text(
-                        listProduct.name,
-                        style: TextStyle(
-                            fontFamily: 'Quicksand',
-                            fontSize: 24,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Card(
+                        color: Colors.blueAccent,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            child: Text(
+                              listProduct.name,
+                              style: TextStyle(
+                                  fontFamily: 'Quicksand',
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
+                    )
+                  ],
                 ),
                 Container(
                   padding: const EdgeInsets.only(right: 10, left: 10),
@@ -57,70 +86,80 @@ class DetailPage extends StatelessWidget {
                     child: Image.asset(listProduct.imageAssets),
                   ),
                 ),
-                Card(
-                  color: Colors.blueAccent,
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    child: Text(
-                      listProduct.description,
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                  ),
-                ),
-                Card(
-                  color: Colors.blueAccent,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Icon(
-                              Icons.monetization_on,
-                              color: Colors.white,
-                              size: 40,
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Card(
+                    color: Colors.blueAccent,
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
+                            child: Text(
+                              listProduct.description,
+                              textAlign: TextAlign.justify,
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
                             ),
-                            Text(listProduct.price,
+                          ),
+                        ),
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.monetization_on,
+                                      color: Colors.white,
+                                      size: 40,
+                                    ),
+                                    Text(listProduct.price,
+                                        style: TextStyle(
+                                            fontSize: 32, color: Colors.white))
+                                  ]),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                  top: 15, right: 20, left: 20, bottom: 10),
+                              child: Text(
+                                'Gratis packing\nGratis karantina\nHarga bisa nego!!!\nGaransi Pengiriman\nPengiriman seluruh Indonesia\nDetail produk lebih lengkap dan negosiasi Klik Tombol dibawah ini!',
+                                textAlign: TextAlign.left,
                                 style: TextStyle(
-                                    fontSize: 32, color: Colors.white))
-                          ]),
-                    ),
-                  ),
-                ),
-                Card(
-                  color: Colors.blueAccent,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      padding: const EdgeInsets.only(
-                          top: 15, right: 20, left: 20, bottom: 10),
-                      child: Text(
-                        'Gratis packing\nGratis karantina\nHarga bisa nego!!!\nGaransi Pengiriman\nPengiriman seluruh Indonesia\nDetail produk lebih lengkap dan negosiasi Klik Tombol dibawah ini!',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontFamily: 'Quicksand',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Contact Us',
-                        style: TextStyle(
-                            fontFamily: 'Quicksand',
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
-                      ),
+                                    fontSize: 16,
+                                    fontFamily: 'Quicksand',
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                _launchUrl();
+                              },
+                              child: Text(
+                                'Order Here!',
+                                style: TextStyle(
+                                    fontFamily: 'Quicksand',
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 )
@@ -129,6 +168,157 @@ class DetailPage extends StatelessWidget {
           ],
         )),
       ),
+    );
+  }
+}
+
+class WebDetailPage extends StatelessWidget {
+  final ListProduct listProduct;
+  const WebDetailPage({Key? key, required this.listProduct}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              'Detail Product',
+              style: TextStyle(
+                fontFamily: 'Quicksand',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const FavoriteButton(),
+          ],
+        ),
+      ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 32),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(right: 10, left: 10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(listProduct.imageAssets),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Card(
+                        color: Colors.blueAccent,
+                        child: Column(
+                          children: [
+                            Container(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Container(
+                                  padding: const EdgeInsets.all(20),
+                                  child: Text(
+                                    listProduct.name,
+                                    style: TextStyle(
+                                        fontFamily: 'Quicksand',
+                                        fontSize: 24,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              color: Colors.blueAccent,
+                              child: Container(
+                                padding: const EdgeInsets.all(20),
+                                child: Text(
+                                  listProduct.description,
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              color: Colors.blueAccent,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Container(
+                                  padding: const EdgeInsets.only(left: 20),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.monetization_on,
+                                          color: Colors.white,
+                                          size: 40,
+                                        ),
+                                        Text(listProduct.price,
+                                            style: TextStyle(
+                                                fontSize: 32,
+                                                color: Colors.white))
+                                      ]),
+                                ),
+                              ),
+                            ),
+                            Container(
+                                color: Colors.blueAccent,
+                                padding: const EdgeInsets.all(20),
+                                child: Container(
+                                  child: Text(
+                                    'Gratis packing, Gratis karantina, Harga bisa nego!!!, Garansi Pengiriman, Pengiriman seluruh Indonesia, Detail produk lebih lengkap dan negosiasi Klik Tombol dibawah ini!',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontFamily: 'Quicksand',
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  ),
+                                )),
+                            const SizedBox(height: 32),
+                            Container(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    _launchUrl();
+                                  },
+                                  child: Text(
+                                    'Order Here!',
+                                    style: TextStyle(
+                                        fontFamily: 'Quicksand',
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )),
     );
   }
 }
@@ -155,5 +345,11 @@ class _FavoriteButtonState extends State<FavoriteButton> {
           isFavorite ? Icons.favorite : Icons.favorite_border,
           color: Colors.red,
         ));
+  }
+}
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw 'Could not launch $_url';
   }
 }
